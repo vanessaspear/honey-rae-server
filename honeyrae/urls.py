39f66a154b5b49d9cd96ceffe9824f885e9642ea
Vanessa Spear <vanessavspear@gmail.com>
2023-01-31 15:45:07 -0600

    Added views for employee, tickets, and customers.  Client can now make get all and get single requests to server.

--- honeyrae/urls.py
+++ honeyrae/urls.py
@@ -1,13 +1,14 @@
 from rest_framework import routers
-
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from repairsapi.views import CustomerView
+from repairsapi.views import CustomerView, EmployeeView, TicketView
 from repairsapi.views import register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'customers', CustomerView, 'customer')
+router.register(r'employees', EmployeeView, 'employee')
+router.register(r'tickets', TicketView, 'ticket')
 
 urlpatterns = [
     path('', include(router.urls)),
---
+++ repairsapi/views/ticket_view.py
@@ -0,0 +1,39 @@
+"""View module for handling requests for ticket data"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from repairsapi.models import ServiceTicket
+
+
+class TicketView(ViewSet):
+    """Honey Rae API tickets view"""
+
+    def list(self, request):
+        """Handle GET requests to get all tickets
+
+        Returns:
+            Response -- JSON serialized list of tickets
+        """
+
+        tickets = ServiceTicket.objects.all()
+        serialized = TicketSerializer(tickets, many=True)
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single ticket
+
+        Returns:
+            Response -- JSON serialized ticket record
+        """
+
+        ticket = ServiceTicket.objects.get(pk=pk)
+        serialized = TicketSerializer(ticket, context={'request': request})
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    """JSON serializer for tickets"""
+    class Meta:
+        model = ServiceTicket
+        fields = ('id', 'description', 'emergency', 'date_completed', 'customer_id', 'employee_id')
